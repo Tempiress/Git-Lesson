64b8e840adb2d419b65bb3b1b5e9c8ad178a13f3
Varlamov Danil <danil.varlamov.03@mail.ru>
2021-10-21 09:33:44 +0300

    Revert "Add func SecondsInMinute in DateTime"
    This reverts commit c49e5e96946d4df892bba184f0435e33879d8a38.

--- DateTime.pas
+++ DateTime.pas
@@ -1,4 +1,4 @@
-﻿ function IsleapYear :integer;
+﻿ function IsleapYear ():integer;
  begin
    var i:string;
  if (datetime.Now.Year mod 4 = 0) and (datetime.Now.Year mod 100 <> 0) and  (datetime.Now.Year mod 400 = 0) then
@@ -8,13 +8,6 @@
  writeln(i);
  end;
  
- function SecondsInMinute:integer;
- begin
-   writeln(datetime.Now.Second);
- end;
- 
 begin
-IsLeapYear;
-SecondsInMinute;
+IsLeapYear();
 end.
-
